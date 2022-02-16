6909698b8669122a972ff1ad1b4b40f94e2c926f
arielsardinha <odararepresentacoes@outlook.com.br>
2022-02-16 16:27:36 -0200

    criando getters

--- pessoa.dart
+++ pessoa.dart
@@ -25,4 +25,20 @@ class Pessoa {
         this._nome = nome,
         this._idade = idade,
         this._email = email;
+
+  String get nome {
+    return this._nome;
+  }
+
+  int get idade {
+    return this._idade;
+  }
+
+  String get sexo {
+    return this._sexo;
+  }
+
+  String get email {
+    return this._email;
+  }
 }
