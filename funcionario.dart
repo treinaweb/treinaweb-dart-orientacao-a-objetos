0a50f11e3c72811086b390c99a58037075ff6392
arielsardinha <odararepresentacoes@outlook.com.br>
2022-02-18 11:28:43 -0200

    fix/Funcionario

--- funcionario.dart
+++ funcionario.dart
@@ -1,7 +1,10 @@
 import 'pessoa.dart';
 
 class Funcionario extends Pessoa {
-  Funcionario(String nome, int idade, String sexo, String email, String cargo,
-      bool vida)
-      : super(nome: nome, idade: idade, sexo: sexo, email: email);
+  Funcionario(
+    String nome,
+    int idade,
+    String sexo,
+    String email,
+  ) : super(nome: nome, idade: idade, sexo: sexo, email: email);
 }
